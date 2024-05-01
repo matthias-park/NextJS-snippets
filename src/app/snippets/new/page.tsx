38cae5c58796a97aace321a25bb9db9b84bd70e5
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-01 12:23:58 -0700

    Feat: Server Actions in Next.js

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,17 +1,33 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // This needs to be a server action!
+    'use server';
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // Create a new record in the database
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+    // Redirect the user back to the root route
+    redirect('/');
+  }
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
           <label className="w-12" htmlFor="title">
             Title
           </label>
-          <input
-            name="tile"
-            className="border rounded p-2 w-full"
-            id="title"
-          />
+          <input name="title" className="border rounded p-2 w-full" id="title" />
         </div>
 
         <div className="flex gap-4">
@@ -26,7 +42,7 @@ export default function SnippetCreatePage() {
         </div>
 
         <button type="submit" className="rounded p-2 bg-blue-200">
-            Create
+          Create
         </button>
       </div>
     </form>
