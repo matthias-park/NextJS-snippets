a6466613dbe9bdfae72976765b978f2d8904ee3c
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-03 09:47:42 -0700

    Feat: handling Editor Changes

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,20 +1,29 @@
-'use client';
+"use client";
 
 import type { Snippet } from "@prisma/client";
-import Editor from '@monaco-editor/react'
+import Editor from "@monaco-editor/react";
+import { useState } from "react";
 
 interface SnippetEditFormProps {
-    snippet: Snippet
+  snippet: Snippet;
 }
 
-export default function SnippetEditForm({snippet}: SnippetEditFormProps) {
-    return <div>
-        <Editor 
-            height="40vh"
-            theme="vs-dark"
-            language="javascript"
-            defaultValue={snippet.code}
-            options={{ minimap: {enabled: false}}}
-        />
+export default function SnippetEditForm({ snippet }: SnippetEditFormProps) {
+  const [code, setCode] = useState(snippet.code);
+
+  const handleEditorChange = (value: string = "") => {
+    setCode(value);
+  };
+  return (
+    <div>
+      <Editor
+        height="40vh"
+        theme="vs-dark"
+        language="javascript"
+        defaultValue={snippet.code}
+        options={{ minimap: { enabled: false } }}
+        onChange={handleEditorChange}
+      />
     </div>
+  );
 }
