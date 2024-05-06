d6c146827ee8456d525a1e5fc0f6f3fc28accd4d
Junsoo Park <matthiaspark0514@gmail.com>
2024-05-06 09:21:00 -0700

    Feat: Deleting a Record with Server Action

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import * as actions from "@/actions";
+
 interface SnippetShowPageProps {
   params: {
     id: string;
@@ -16,13 +18,22 @@ export default async function SnippetShowPage(props: SnippetShowPageProps) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
-          <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-          <button className="p-2 border rounded">Delete</button>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
